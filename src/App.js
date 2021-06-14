1af89098db8a48fdb8472d8abc45232768551041
Ramiro <ramegp@ramegp>
2021-06-14 17:42:55 -0300

    remove logo, import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MenuDesktop from "./components/Menu/Desktop/MenuDesktop";
@@ -15,6 +14,7 @@ import Shop from './pages/Shop/Shop'
 
 import { Hidden } from '@material-ui/core';
 import About from "./pages/About/About";
+import CheckoutPage from './pages/Checkout/Checkout'
 
 function App() {
   return (
@@ -26,6 +26,7 @@ function App() {
           <Route exact path="/home" component={Home}/>
           <Route exact path="/shop" component={Shop}/>
           <Route exact path="/about" component={About}/>
+          <Route exact path="/checkout-page" component={CheckoutPage}/>
         </Switch>
         
       </Router>
